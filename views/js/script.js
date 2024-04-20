fafc5e8a4bb903c8fdcd744a47fa6502a11e5b9c
Mustafa Y. Yılmaz <92305184+mustafay-yilmaz@users.noreply.github.com>
2024-04-20 14:31:15 +0300

    new

--- views/js/script.js
+++ views/js/script.js
@@ -1,4 +1,10 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const divElement = document.getElementById('gezilecekYerler');
+    
+    divElement.addEventListener('click', function() {
+        window.location.href = '../popdest';
+    });
+});
 
   
        
