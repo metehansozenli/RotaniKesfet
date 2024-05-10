67b0452872bca2b92631b41d6a08b5b4b11a9ce7
Mustafa Y. Yılmaz <92305184+mustafay-yilmaz@users.noreply.github.com>
2024-05-10 03:50:54 +0300

    routeplanner güncelleme

--- views/js/citylocationDataLoader.js
+++ views/js/citylocationDataLoader.js
@@ -2,12 +2,9 @@ var j = 1;
 var i = 1;
 
 document.addEventListener('DOMContentLoaded', function () {
-    citylocation_load_data().then(() => {
-        console.log(dropdownsContainer.innerHTML);
-    }).catch(error => {
-        console.error('citylocation_load_data fonksiyonunda bir hata oluştu:', error);
-    });
+    citylocation_load_data();
 });
+
 const citylocation_load_data = () => {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
@@ -15,50 +12,61 @@ const citylocation_load_data = () => {
         request.onload = () => {
             results = JSON.parse(request.responseText);
             let html = '';
-            let html2 = '';
-            
+            let promises = []; // Asenkron işlemleri takip etmek için bir dizi oluştur
+
             if (results.length > 0) {
                 results.forEach(result => {
                     const request2 = new XMLHttpRequest();
                     request2.open('GET', `/get_typelocationData?locationType=${result.locationType}`);
-                    request2.onload = () => {
-                        const results2 = JSON.parse(request2.responseText);
-                        const list = ".checkboxLocations"+(i++);
-                        const listItems = document.querySelector(list);
-                        results2.forEach(result2 => {
-                            html2 += `
-                                <li class="item">
-                                    <span class="checkbox">
-                                        <i class="fa-solid fa-check check-icon"></i>
-                                    </span>
-                                    <span class="item-text">${result2.locationName}</span>
-                                </li>
-                            `;
-                        });
-                    };
-                    request2.onerror = () => {
-                        reject('İkinci istek başarısız');
-                    };
-                    request2.send();
-                    
-                    html += `
-                        <div class="dropdowns-container">
-                            <div class="calendar-category-container">
-                                <div class="select-btn" id="select-btn">
-                                    <span class="btn-text">${result.locationType}</span>
-                                    <span class="arrow-dwn">
-                                        <i class="fa-solid fa-chevron-down"></i>
-                                    </span>
+                    let html2 = ''; // html2'yi içerde tanımla
+
+                    // Asenkron işlemi takip et
+                    let promise = new Promise((resolve, reject) => {
+                        request2.onload = () => {
+                            const results2 = JSON.parse(request2.responseText);
+                            results2.forEach(result2 => {
+                                html2 += `
+                                    <li class="item">
+                                        <span class="checkbox">
+                                            <i class="fa-solid fa-check check-icon"></i>
+                                        </span>
+                                        <span class="item-text">${result2.locationName}</span>
+                                    </li>
+                                `;
+                            });
+
+                            // html2'yi burada oluşturup, html içine ekle
+                            html += `
+                                <div class="dropdowns-container">
+                                    <div class="calendar-category-container">
+                                        <div class="select-btn" id="select-btn">
+                                            <span class="btn-text">${result.locationType}</span>
+                                            <span class="arrow-dwn">
+                                                <i class="fa-solid fa-chevron-down"></i>
+                                            </span>
+                                        </div>
+                                        <ul class="list-items categoryItems checkboxLocations`+ (j++) +`">${html2}</ul>
+                                    </div>
                                 </div>
-                                <ul class="list-items categoryItems checkboxLocations`+ (j++) +`">`+ html2 +`</ul>
-                            </div>
-                        </div>
-                    `;
-                    
+                            `;
+                            resolve(); // Asenkron işlem tamamlandığında Promise'i çöz
+                        };
+                        request2.onerror = () => {
+                            reject('İkinci istek başarısız');
+                        };
+                        request2.send();
+                    });
+                    promises.push(promise); // Promise'i diziye ekle
                 });
-                dropdownsContainer.innerHTML = dropdownsContainer.innerHTML + html;
-                document.dispatchEvent(new CustomEvent('customDropdownEventListener'));
-                resolve(); // İşlem tamamlandığında Promise'i çöz
+
+                // Tüm asenkron işlemler tamamlandığında devam et
+                Promise.all(promises).then(() => {
+                    dropdownsContainer.innerHTML = dropdownsContainer.innerHTML + html;
+                    document.dispatchEvent(new CustomEvent('customDropdownEventListener'));
+                    resolve(); // İşlem tamamlandığında Promise'i çöz
+                }).catch(error => {
+                    reject(error); // Hata oluştuğunda Promise'i reddet
+                });
             } else {
                 resolve(); // İşlem tamamlandığında Promise'i çöz
             }
@@ -67,35 +75,40 @@ const citylocation_load_data = () => {
             reject('İlk istek başarısız');
         };
         request.send();
-        
     });
 };
 
 
 
+
 document.addEventListener('customDropdownEventListener', function () {
-    var selectBtn = document.querySelectorAll("#select-btn")
-    selectBtn.forEach(selectBtnElements => {
-        
-        items = document.querySelectorAll("#tuna");
+    var selectBtns = document.querySelectorAll(".select-btn"); // Tüm dropdown seçim düğmelerini seç
+
+    selectBtns.forEach(selectBtn => {
+        selectBtn.addEventListener("click", () => {
+            selectBtn.classList.toggle("open");
+        });
+
+        var items = selectBtn.nextElementSibling.querySelectorAll(".item"); // Her dropdown için öğeleri seç
         items.forEach(item => {
             item.addEventListener("click", () => {
                 item.classList.toggle("checked");
-
-                let checked = document.querySelectorAll(".calendar-category-container .checked"),
-                    btnText = document.querySelector(".btn-text");
-
-                if (checked && checked.length > 0) {
-                    btnText.innerText = `${checked.length} Seçildi`;
-                } else {
-                    btnText.innerText = "Puan Seç";
-                }
+                updateButtonText(selectBtn); // Dropdown düğmesinin metnini güncelle
             });
-        })
-        selectBtnElements.addEventListener("click", () => {
-        selectBtnElements.classList.toggle("open");
         });
     });
-    
+
+    // Dropdown düğmesinin metnini güncelleyen yardımcı fonksiyon
+    function updateButtonText(btn) {
+        let checkedItems = btn.nextElementSibling.querySelectorAll(".checked");
+        let btnText = btn.querySelector(".btn-text");
+
+        if (checkedItems.length > 0) {
+            btnText.innerText = `${checkedItems.length} Seçildi`;
+        } else {
+            btnText.innerText = "Puan Seç";
+        }
+    }
 });
+
 
