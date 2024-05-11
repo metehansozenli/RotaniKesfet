b08c1175eaf122ddd6592bcf18565c741daee83f
y-eren <kynxaresopollon@gmail.com>
2024-05-11 13:19:28 +0300

    index kullanıcı

--- controllers/iController.js
+++ controllers/iController.js
@@ -1,10 +1,11 @@
 const veritabani = require("./indexController")
 exports.getIndexController = async (req, res) => {
     try {
+      const userID = req.session.userID;
       const randomCitiesData = await veritabani.getRandomCitiesData();
       const randomCitiesData2 = await veritabani.getRandomCitiesData();
       const restaurantData = await veritabani.getRestaurantData();
-      res.render("index",  { randomCitiesData: randomCitiesData, randomCitiesData2 : randomCitiesData2, restaurantData:restaurantData});
+      res.render("index",  { randomCitiesData: randomCitiesData, randomCitiesData2 : randomCitiesData2, restaurantData:restaurantData, userID :userID});
     } catch (error) {
       console.error("index acilirken hata olustu:", error);
       res.render("page404");
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const session = require("express-session"); // express-session middleware'ini ekledik
+
 
 const getIndexController = require("../controllers/iController")
-
+router.use(session({
+    secret : "gizli_kelime",
+    resave : false,
+    saveUninitialized : false
+  }));
 router.get("/", getIndexController.getIndexController)
+
+
 
 module.exports = router
